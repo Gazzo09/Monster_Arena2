eced7d364384a75a9aa375c9b3f4edb2d9a2781a
TomMorol <thomas.morolli09@gmail.com>
2026-05-25 20:07:08 +0200

    Finestra pygame

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
 print("Monster_arena)
+
+import pygame
+
+pygame.init()
+
+WIDTH = 1000
+HEIGHT = 500
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+pygame.display.set_caption("Dino Shooter")
+
+clock = pygame.time.Clock()
+
+running = True
+
+while running:
+
+    clock.tick(60)
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((30, 30, 30))
+
+    pygame.display.flip()
+
+pygame.quit()
       
