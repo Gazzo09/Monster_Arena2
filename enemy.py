7ddb7475c879ddbd3102430db7590d845e3c2a8d
Gazzo09 <francescogazzoni09@gmail.com>
2026-05-26 17:42:37 +0200

    Zombie4

--- enemy.py
+++ enemy.py
@@ -9,90 +9,93 @@ class Enemy:
         self.x = 1000
         self.speed = 4
 
+        # --- DIMENSIONI INGRANDITE ---
         if self.type == "ground":
-            self.y = 350
-            self.width = 40
-            self.height = 40
+            self.y = 330        # Abbassato leggermente per compensare l'altezza
+            self.width = 60     # Ingrandito (da 40 a 60)
+            self.height = 60    # Ingrandito (da 40 a 60)
             self.hp = 200
 
         elif self.type == "fly":
-            self.y = 250
-            self.width = 40
-            self.height = 40
+            self.y = 230        # Alzato per farlo volare meglio
+            self.width = 60     # Ingrandito (da 40 a 60)
+            self.height = 60    # Ingrandito (da 40 a 60)
             self.hp = 100
 
-        else:
-            self.y = 300
-            self.width = 80
-            self.height = 80
+        else: # Boss
+            self.y = 260        # Posizionato per la stazza del Boss
+            self.width = 120    # Ingrandito (da 80 a 120)
+            self.height = 120   # Ingrandito (da 80 a 120)
             self.hp = 400
             self.speed = 2  # boss lento
 
-        # Genera la grafica in pixel art senza file esterni
+        # Genera la grafica degli UFO in pixel art
         self.image = self.generate_pixel_art()
 
     def generate_pixel_art(self):
-        # 1. Definiamo i colori (RGB)
-        _ = (0, 0, 0, 0)          # Trasparente
-        V = (34, 139, 34)         # Verde pelle zombie
-        B = (70, 130, 180)        # Blu vestito
-        P = (47, 79, 79)          # Grigio pantaloni
-        R = (255, 0, 0)           # Rosso (occhi o dettagli)
-        N = (0, 0, 0)             # Nero pupilla
-        G = (140, 140, 140)       # Grigio (per le ali del tipo "fly")
+        # Palette Colori UFO / Alieni
+        _ = (0, 0, 0, 0)            # Trasparente
+        G = (128, 128, 128)         # Grigio metallo UFO
+        C = (0, 255, 255)           # Ciano (Vetro cabina / Raggio)
+        R = (255, 0, 0)             # Rosso (Luci di segnalazione)
+        V = (50, 205, 50)           # Verde Lime (Corpo Alieno)
+        N = (0, 0, 0)               # Nero (Occhi alieno)
+        O = (255, 165, 0)           # Arancione (Propulsori / Dettagli)
 
-        # 2. Definiamo i pattern 8x8 per tipo di nemico
-        if self.type == "ground":
+        # Griglie 8x8 a tema UFO ed Alieni
+        if self.type == "ground": # Piccolo Alieno in capsula terrestre
             grid = [
-                [_, _, V, V, V, V, _, _],
-                [_, V, R, V, V, R, V, _],
-                [_, V, V, V, V, V, V, _],
-                [_, _, B, B, B, B, _, _],
-                [_, B, B, B, B, B, B, _],
-                [_, _, P, P, P, P, _, _],
-                [_, _, P, _, _, P, _, _],
-                [_, V, V, _, _, V, V, _]
-            ]
-        elif self.type == "fly":
-            grid = [
-                [G, _, _, V, V, _, _, G],
-                [G, G, V, R, R, V, G, G],
-                [_, G, V, V, V, V, G, _],
-                [_, _, B, B, B, B, _, _],
-                [_, _, B, B, B, B, _, _],
-                [_, _, P, P, P, P, _, _],
                 [_, _, _, V, V, _, _, _],
-                [_, _, _, _, _, _, _, _]
+                [_, _, N, V, V, N, _, _],
+                [_, _, _, V, V, _, _, _],
+                [_, _, C, C, C, C, _, _],
+                [_, G, G, G, G, G, G, _],
+                [G, G, O, G, G, O, G, G],
+                [_, _, G, _, _, G, _, _],
+                [_, O, _, _, _, _, O, _]
             ]
-        else: # Boss (Zombie gigante arrabbiato)
+        elif self.type == "fly": # UFO Classico Volante
             grid = [
-                [_, V, V, V, V, V, V, _],
-                [V, V, V, V, V, V, V, V],
-                [V, N, R, V, V, R, N, V],
-                [V, V, V, R, R, V, V, V],
-                [_, B, B, B, B, B, B, _],
-                [B, B, B, B, B, B, B, B],
-                [_, P, P, P, P, P, P, _],
-                [_, P, P, _, _, P, P, _]
+                [_, _, _, C, C, _, _, _],
+                [_, _, C, C, C, C, _, _],
+                [_, G, G, G, G, G, G, _],
+                [G, R, G, R, G, R, G, R],
+                [G, G, G, G, G, G, G, G],
+                [_, _, C, C, C, C, _, _],
+                [_, _, C, _, _, C, _, _],
+                [_, _, C, _, _, C, _, _]
+            ]
+        else: # Boss: Nave Madre UFO Gigante
+            grid = [
+                [_, _, _, _, G, G, _, _, _, _],
+                [_, _, _, G, C, C, G, _, _, _],
+                [_, _, G, G, C, C, G, G, _, _],
+                [_, G, G, G, G, G, G, G, G, _],
+                [G, R, G, R, G, R, G, R, G, R],
+                [G, G, G, G, G, G, G, G, G, G],
+                [_, G, O, O, G, G, O, O, G, _],
+                [_, _, G, G, G, G, G, G, _, _]
             ]
 
-        # 3. Creiamo una superficie quadrata temporanea (8x8 pixel)
-        surf_8x8 = pygame.Surface((8, 8), pygame.SRCALPHA)
+        # Determina la dimensione della griglia sorgente (il boss usa 10x8 per essere più largo)
+        grid_width = len(grid[0])
+        grid_height = len(grid)
+
+        # Creiamo la superficie pixel art nativa
+        surf_native = pygame.Surface((grid_width, grid_height), pygame.SRCALPHA)
         
-        # Disegniamo i pixel sulla superficie 8x8
         for y, row in enumerate(grid):
             for x, color in enumerate(row):
-                surf_8x8.set_at((x, y), color)
+                surf_native.set_at((x, y), color)
 
-        # 4. Scaliamo la superficie 8x8 alle dimensioni reali del nemico (width, height)
-        final_surf = pygame.transform.scale(surf_8x8, (self.width, self.height))
+        # Scaliamo la superficie alle nuove dimensioni ingrandite
+        final_surf = pygame.transform.scale(surf_native, (self.width, self.height))
         return final_surf
 
     def move(self):
         self.x -= self.speed
 
     def draw(self, screen):
-        # Disegna lo zombie generato a schermo
         screen.blit(self.image, (self.x, self.y))
 
     def rect(self):
