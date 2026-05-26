25bcc82e2e2e13c32aca515f449a3fbe0db7347b
Gazzo09 <francescogazzoni09@gmail.com>
2026-05-26 17:27:13 +0200

    Zombie

--- enemy.py
+++ enemy.py
@@ -6,9 +6,7 @@ class Enemy:
     def __init__(self, level):
 
         self.type = random.choice(["ground", "fly", "boss"])
-
         self.x = 1000
-
         self.speed = 4
 
         if self.type == "ground":
@@ -16,12 +14,18 @@ class Enemy:
             self.width = 40
             self.height = 40
             self.hp = 200
+            # Carica l'immagine dello zombie a terra e la ridimensiona (40x40)
+            self.image = pygame.image.load("zombie_ground.png").convert_alpha()
+            self.image = pygame.transform.scale(self.image, (self.width, self.height))
 
         elif self.type == "fly":
             self.y = 250
             self.width = 40
             self.height = 40
             self.hp = 100
+            # Carica lo zombie volante o pipistrello (40x40)
+            self.image = pygame.image.load("zombie_fly.png").convert_alpha()
+            self.image = pygame.transform.scale(self.image, (self.width, self.height))
 
         else:
             self.y = 300
@@ -29,25 +33,16 @@ class Enemy:
             self.height = 80
             self.hp = 400
             self.speed = 2  # boss lento
+            # Carica il boss zombie gigante (80x80)
+            self.image = pygame.image.load("zombie_boss.png").convert_alpha()
+            self.image = pygame.transform.scale(self.image, (self.width, self.height))
 
     def move(self):
         self.x -= self.speed
 
     def draw(self, screen):
-
-        color = (255, 0, 0)
-
-        if self.type == "fly":
-            color = (0, 0, 255)
-
-        if self.type == "boss":
-            color = (120, 0, 120)
-
-        pygame.draw.rect(
-            screen,
-            color,
-            (self.x, self.y, self.width, self.height)
-        )
+        # Sostituito pygame.draw.rect con screen.blit per disegnare l'immagine
+        screen.blit(self.image, (self.x, self.y))
 
     def rect(self):
         return pygame.Rect(self.x, self.y, self.width, self.height)
